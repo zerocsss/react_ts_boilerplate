e3a9b54a87a8114c3faa4cbfb122701334c9bc59
zerocsss <1756297001@qq.com>
2021-06-03 13:34:14 +0800

    Keep calm and commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><img src={logo} className="App-logo" alt="logo" /><p>
-          Edit <code>src/App.tsx</code> 565566 save to reload.</p>
-        <a className="App-link"
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> 22222 save to reload.
+        </p>
+        <a
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
