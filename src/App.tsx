1f5a704b72c97a68cbca41f2f3bdb7ebe65712f8
zerocsss <1756297001@qq.com>
2021-06-03 13:24:08 +0800

    Keep calm and commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"><img src={logo} className="App-logo" alt="logo" /><p>
-          Edit <code>src/App.tsx</code> 22222222 save to reload.</p>
-        <a
-          className="App-link"
+          Edit <code>src/App.tsx</code> 444444 save to reload.</p>
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
