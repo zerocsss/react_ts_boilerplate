59dea76cdec00fecc909a03faa04513bb304289b
zerocsss <1756297001@qq.com>
2021-06-04 16:47:57 +0800

    hotfix

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,19 @@
+import React, { FC, ReactElement } from 'react'
+
+// export default function Loading(): ReactElement {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+const Loading: FC = () => {
+  return (
+    <div>
+      loading...
+    </div>
+  )
+}
+
+export default Loading
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,37 @@
-import {FC} from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { FC } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import loadable from "@loadable/component";
+import Loading from "components/Loading/Loading";
+
+const RouterList = [
+  {
+    component: () => import(""),
+    path: "/total_statistic",
+  },
+];
 
 const RouteIndex: FC = () => {
   return (
     <Router>
-    
-    </Router>  
-  )
-}
+      <Switch>
+        {RouterList.map((item) => (
+          <Route
+            key={item.path}
+            exact={item.path === "/cubes" ? true : false}
+            path={item.path}
+            component={loadable<any>(item.component, {
+              fallback: <Loading />,
+            })}
+          />
+        ))}
+      </Switch>
+    </Router>
+  );
+};
+
+export default RouteIndex;
