b8d1821d94a5439fc3f2a0f518757b50fa2c2f33
zerocsss <1756297001@qq.com>
2021-06-04 14:04:14 +0800

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Button, DatePicker } from "antd";
-// import "./App.less";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <DatePicker></DatePicker>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Button type="primary">Button</Button>
-          Edit <code>src/App.tsx</code> 22222 save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">React 基础模板</div>;
 }
 
 export default App;
---
+++ src/router/index.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+const RouteIndex: FC = () => {
+  return (
+    <Router>
+    
+    </Router>  
+  )
+}
