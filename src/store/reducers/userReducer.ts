4a024cfb629dd652974ca117b49dc74340d881a9
zerocsss <1756297001@qq.com>
2021-06-04 11:08:00 +0800

    update reducer

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,30 @@
+interface UserState {
+  userInfo: {
+    id: number,
+    name: string,
+  }
+}
+interface StoreAction {
+  type: string;
+  payload: any;
+}
+
+const initState: UserState = {
+  userInfo: {
+    id: 0,
+    name: '',
+  }
+}
+
+const userReducer = (state=initState, action:StoreAction):object => {
+  switch(action.type) {
+    case 'SAVE_USER_INFO':
+      return {
+        ...state,
+      }
+    default:
+      return {}
+  }
+}
+
+export {userReducer}
